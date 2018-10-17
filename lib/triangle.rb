ff0fa73dceeaebc4d66aef37ecab697c9085a9e1
magic-onion <alexander.m.truong@gmail.com>
2018-10-17 17:05:20 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_writer :kind
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    self.real_triangle
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+      :isosceles
+    else @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+      :scalene
+    end
+  end
+
+  def real_triangle
+    if @side1 == 0 || @side2 == 0 || @side3 == 0
+      raise TriangleError
+    elsif @side1 < 0 || @side2 < 0|| @side3 < 0
+      raise TriangleError
+    elsif (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
+      raise TriangleError
+    end
+
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
