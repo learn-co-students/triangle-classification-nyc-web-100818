d8be02855f6a221742ea53d22a126c3abcd68325
magic8baller <lturis@gmail.com>
2018-10-17 17:16:50 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,6 @@ class Triangle
       raise TriangleError
     else
 
-
       if side1 == side2 && side2 == side3
         return :equilateral
       elsif side1 != side2 && side1 != side3 && side2 != side3 
